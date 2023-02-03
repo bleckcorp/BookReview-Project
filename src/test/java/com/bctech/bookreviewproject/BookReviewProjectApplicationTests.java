8bb942e7406758340aa537a1f98c92bbfb93c0f4
Blessing Chukwubuogum <59502310+bleckcorp@users.noreply.github.com>
2023-02-03 10:12:17 +0100

    Initial commit

---
+++ src/test/java/com/bctech/bookreviewproject/BookReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bctech.bookreviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
